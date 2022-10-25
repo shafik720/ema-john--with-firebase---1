8337dfeccc8e60cc5c532abf3c374b33b492ab33
shafik720 <shafikrasel5@gmail.com>
2022-10-25 14:34:13 +0600

    fgdsgd

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -2,16 +2,26 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
-    console.log(props);
+
+    let totalPrice = 0;
+    let shipping = 0;
+    let grandTotal = 0;
+    let tax = 0 ;
+    for(let element of props.cart){
+        totalPrice += element.price;
+        shipping += element.shipping;       
+    }
+    tax += (totalPrice * 0.1);
+    grandTotal = tax + totalPrice;
     return (
         <div className='cart-div'>
             <div className="cart-div2">
                 <h2>Order Summary :  </h2>
                 <h3>Product Added : {props.cart.length} </h3>
-                <h3>Total Price : </h3>
-                <h3>Shipping Fee : </h3>
-                <h3>Tax : </h3>
-                <h3>Grand Total : </h3>
+                <h3>Total Price : {totalPrice} </h3>
+                <h3>Shipping Fee : {shipping} </h3>
+                <h3>Tax : {tax.toFixed(2)} </h3>
+                <h3>Grand Total : {grandTotal} </h3>
             </div>
             
         </div>
