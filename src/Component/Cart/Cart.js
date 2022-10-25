04c80ce1c1de295231a5cccae5f3dee2b01250ee
shafik720 <shafikrasel5@gmail.com>
2022-10-25 14:15:26 +0600

    fgdsgd

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -8,6 +8,10 @@ const Cart = (props) => {
             <div className="cart-div2">
                 <h2>Order Summary :  </h2>
                 <h3>Product Added : {props.cart.length} </h3>
+                <h3>Total Price : </h3>
+                <h3>Shipping Fee : </h3>
+                <h3>Tax : </h3>
+                <h3>Grand Total : </h3>
             </div>
             
         </div>
