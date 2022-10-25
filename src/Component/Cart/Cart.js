3ce380dff72892d13a54ff6efddbd2a87d8df29d
shafik720 <shafikrasel5@gmail.com>
2022-10-25 12:47:50 +0600

    fgdsgd

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
     return (
-        <div>
-            <h2>Hello From Cart : {props.cart.length} </h2>
+        <div className='cart-div'>
+            <div className="cart-div2">
+                <h2>Hello From Cart : {props.cart.length} </h2>
+            </div>
+            
         </div>
     );
 };
