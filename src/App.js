6fa096400b0e26d879175fb740bfbd42fd3800f2
shafik720 <shafikrasel5@gmail.com>
2022-11-01 22:35:02 +0600

    dfgdsfg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { Route, Routes } from 'react-router';
 import './App.css';
 import Header from './Component/Header/Header';
 import Shop from './Component/Shop/Shop';
@@ -7,6 +8,10 @@ function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Shop></Shop>}></Route>
+        {/* <Route path="*" element={}></Route> */}
+      </Routes>
       <Shop></Shop>
     </div>
   );
