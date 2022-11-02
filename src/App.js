547b7484fdfca67010104ffab40db74e4ed0e117
shafik720 <shafikrasel5@gmail.com>
2022-11-02 19:17:09 +0600

    dfgdg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { createContext } from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Cart from './Component/Cart/Cart';
@@ -7,9 +8,14 @@ import Header from './Component/Header/Header';
 import Shop from './Component/Shop/Shop';
 import ShopCart from './Component/ShopCart/ShopCart';
 
+
+export const DummyContext = createContext('dummy');
+
 function App() {
+  let x = 420;
   return (
-    <div>
+    <DummyContext.Provider value={x}>
+      <div>
       <Header></Header>
       <Routes>
         <Route path="/" element={<Shop></Shop>}></Route>
@@ -17,6 +23,8 @@ function App() {
         <Route path="*" element={<Error></Error>}></Route>
       </Routes>
     </div>
+    </DummyContext.Provider>
+    
   );
 }
 
