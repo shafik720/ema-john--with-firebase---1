bdc95186fba9edcd8a14e955c453d261fcac59da
shafik720 <shafikrasel5@gmail.com>
2022-11-14 22:39:15 +0600

    working on register page

---
+++ src/notes_step.js
@@ -0,0 +1,9 @@
+/* 
+    1. Create a new Project in console.google.com
+    2. npm install firebase
+    3. create.firebase.init.js in vsCode and import getAuth to export Auth.
+    4. firebase settings > authentication > enable email & password auth
+    5. Create login, signUp component, setup their route
+    6. attach signup & login form's all input field's handler and form submit prevent default handler
+    7. 
+*/
