d011865baaffa9d2d6bf4d314dbac382784f7965
shafik720 <shafikrasel5@gmail.com>
2022-11-14 22:13:41 +0600

    working on register page

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -3,27 +3,44 @@ import './Register.css';
 import googleLogo from "../../utilities/google.svg"
 import { Link } from 'react-router-dom';
 import Login from '../Login/Login';
+import { useState } from 'react';
 
 const Register = () => {
+    let[email, setEmail] = useState('');
+    let[password, setPassword] = useState('');
+    let[confirmPassword, setConfirmPassword] = useState('');
+    // let[] = useState('');
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+    function handleConfirmPassword(e){
+        setConfirmPassword(e.target.value);
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+    }
     return (
         <div className='my-5'>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6 mx-auto">
-                        <form action="">
+                        <form action="" onSubmit={handleSubmit}>
                         <div className="login-div mx-auto">
                             <h2>Register</h2>
                             <div className="email-div">
                                 <p>Email</p>
-                                <input type="email" name="" id="" />
+                                <input onBlur={handleEmail} type="email" name="" id="" />
                             </div>
                             <div className="password-div">
                                 <p>Password</p>
-                                <input type="password" name="" id="" />
+                                <input onBlur={handlePassword} type="password" name="" id="" />
                             </div>
                             <div className="password-div">
                                 <p>Confirm Password</p>
-                                <input type="password" name="" id="" />
+                                <input onBlur={handleConfirmPassword} type="password" name="" id="" />
                             </div>
                             <div className="login-button">
                                 <button>Sign Up</button>
