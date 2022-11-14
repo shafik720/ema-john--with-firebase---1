84465c2f285e54c1c10990c7e33dc7154835976c
shafik720 <shafikrasel5@gmail.com>
2022-11-14 20:35:27 +0600

    working on login page

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -3,12 +3,28 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div className='login-div'>
+        <div className='my-5'>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6">
-                        <div className="">
-                        <h2>Login</h2>
+                        <div className="login-div">
+                            <h1>Log In</h1>
+                            <div className="email-div">
+                                <p>Email</p>
+                                <input type="email" name="" id="" />
+                            </div>
+                            <div className="password-div">
+                                <p>Password</p>
+                                <input type="password" name="" id="" />
+                            </div>
+                            <div className="login-button">
+                                <button>Login</button>
+                                <p>New to Ema-John ? <a href="#">Create New Account</a> </p>
+                            </div>
+                            <span>Or</span>
+                            <div className="google-div">
+                                Continue With Google
+                            </div>
                         </div>
                     </div>
                 </div>
