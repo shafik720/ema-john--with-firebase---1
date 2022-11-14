252325fdcc403722d7e8238b744b2ab6000f1058
shafik720 <shafikrasel5@gmail.com>
2022-11-14 20:30:24 +0600

    initializing login and sign up route

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -3,8 +3,16 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='login-div'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="">
+                        <h2>Login</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
