787f5225f65c4f1667d3aea554b04cb6045f5bed
shafik720 <shafikrasel5@gmail.com>
2022-11-15 19:41:03 +0600

    implementing protected authentication in  other route

--- src/notes_step.js
+++ src/notes_step.js
@@ -5,5 +5,11 @@
     4. firebase settings > authentication > enable email & password auth
     5. Create login, signUp component, setup their route
     6. attach signup & login form's all input field's handler and form submit prevent default handler
-    7. npm install --save react-firebase-hooks
+    7. npm install --save react-firebase-hooks (from github firebase hook page)
+    8. useCreateUserWithEmailAndPassword from react-firebase-hook (from 7number line page)
+    9. if user is created, redirect them to the expected page
+    10. useSignInWithEmailAndPassword for sign in
+    11. create RequireAuthComponent (from https://stackblitz.com/github/remix-run/react-router/tree/main/examples/auth?file=src%2FApp.tsx) check if user exists and also track user's location
+    12. make changes in login.js with RequireAuthFunction.
+    13. in app.js file under the <Route>, wrap the desired route with <Requireauth> component.
 */
