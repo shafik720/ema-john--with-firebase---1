373320cda6d1a60c34ba1b44352e59ca38eb6e88
shafik720 <shafikrasel5@gmail.com>
2022-11-15 19:23:16 +0600

    implementing other route

---
+++ src/Component/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Contact = () => {
+
+    return (
+        <div className='text-center'>
+            <h2>Have Questions ? <br /> Contact Us</h2>            
+        </div>
+    );
+};
+
+export default Contact;
