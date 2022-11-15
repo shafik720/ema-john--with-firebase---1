8429515c8eb929995522ce78f073baa856c281ff
shafik720 <shafikrasel5@gmail.com>
2022-11-15 18:12:33 +0600

    working login js

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -8,12 +8,12 @@ import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 
 const Login = () => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    function handleEmail(e){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    function handleEmail(e) {
         setEmail(e.target.value);
     }
-    function handlePassword(e){
+    function handlePassword(e) {
         setPassword(e.target.value);
     }
     const [
@@ -21,32 +21,34 @@ const Login = () => {
         user,
         loading,
         error,
-      ] = useSignInWithEmailAndPassword(auth);
+    ] = useSignInWithEmailAndPassword(auth);
     return (
         <div className='my-5'>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6 mx-auto">
-                        <div className="login-div mx-auto">
-                            <h2>Log In</h2>
-                            <div className="email-div">
-                                <p>Email</p>
-                                <input onBlur={handleEmail} type="email" name="" id="" required/>
+                        <form action="">
+                            <div className="login-div mx-auto">
+                                <h2>Log In</h2>
+                                <div className="email-div">
+                                    <p>Email</p>
+                                    <input onBlur={handleEmail} type="email" name="" id="" required />
+                                </div>
+                                <div className="password-div">
+                                    <p>Password</p>
+                                    <input onBlur={handlePassword} type="password" name="" id="" required />
+                                </div>
+                                <div className="login-button">
+                                    <button>Login</button>
+                                    <p>New to Ema-John ? <Link to="/register" element={<Register></Register>}>Create New Account</Link> </p>
+                                </div>
+                                <span><h5>Or</h5></span>
+                                <div draggable className="google-div">
+                                    <img src={googleLogo} alt="" />
+                                    Continue With Google
+                                </div>
                             </div>
-                            <div className="password-div">
-                                <p>Password</p>
-                                <input onBlur={handlePassword} type="password" name="" id="" required/>
-                            </div>
-                            <div className="login-button">
-                                <button>Login</button>
-                                <p>New to Ema-John ? <Link to="/register" element={<Register></Register>}>Create New Account</Link> </p>
-                            </div>
-                            <span><h5>Or</h5></span>
-                            <div draggable className="google-div">
-                                <img src={googleLogo} alt="" />
-                                Continue With Google
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
